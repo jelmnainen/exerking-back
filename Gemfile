93eda099028da231d491bb1b2447a038171cd073
Sami Åhl <sami.leo.ahl@gmail.com>
2016-01-28 16:17:44 +0200

    implemented basic authentication

--- Gemfile
+++ Gemfile
@@ -24,6 +24,10 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 # Added
 gem 'therubyracer', platforms: :ruby
 gem 'rspec-rails', '~> 3.0'
+gem 'rails-api', '~> 0.4.0'
+gem 'active_model_serializers', '~> 0.8.3' # NOTE: not the 0.9
+gem 'devise', '~> 3.4.1'
+
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,42 @@
-class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+class ApplicationController < ActionController::API
+	#include AbstractController::Translation
+	# Prevent CSRF attacks by raising an exception.
+	# For APIs, you may want to use :null_session instead.
+	# protect_from_forgery :null_session
+	before_action :authenticate_user_from_token!
+	respond_to :json
+
+	def authenticate_user_from_token!
+  		auth_header = request.headers['Authorization'] || ""
+  		auth_type, auth_token = auth_header.split(" ")
+
+		if auth_token && auth_type == "Bearer"
+			authenticate_with_auth_token auth_token
+		else
+			authentication_error
+		end
+	end
+
+  	private
+
+  	def authenticate_with_auth_token auth_token
+  		unless auth_token.include?(':')
+  			authentication_error
+  			return
+  		end
+
+  		user_id = auth_token.split(':').first
+  		user = User.where(id: user_id).first
+
+  		if user && Devise.secure_compare(user.access_token, auth_token)
+  			sign_in user, store: false
+  		else
+  			authentication_error
+  		end
+  	end
+
+  	def authentication_error
+  		render json: {error: 'unauthorized'}, status: 401
+  	end
+
 end
