2a8cbe75bddea8f39ce426b49bcfcd05668282cb
Johannes Elmnäinen <sanhologin@gmail.com>
2016-02-04 16:40:09 +0200

    Updates exercise tests

--- spec/requests/exercises_spec.rb
+++ spec/requests/exercises_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "Exercises", type: :request do
   describe "GET /exercises" do
     it "works! (now write some real specs)" do
       get exercises_path
-      expect(response).to have_http_status(401)
+      expect(response).to have_http_status(200)
     end
   end
 end
