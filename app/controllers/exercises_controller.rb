9e3bbc2f92baa470e03f50765b289e4f178086cd
tme <theme87@gmail.com>
2016-02-10 14:25:47 +0200

    ExercisesCtrl fix

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,4 +1,4 @@
-class ExercisesController < ApplicationController::Base
+class ExercisesController < ApplicationController
   #load_and_authorize_resource
   before_action :set_exercise, only: [:show, :edit, :update, :destroy]
 
