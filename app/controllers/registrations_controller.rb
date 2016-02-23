be8f59e078000262d9287e1b8d59ab6174eee353
tme <theme87@gmail.com>
2016-02-23 17:06:00 +0200

    removed devise root element

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,4 +1,10 @@
 class RegistrationsController < Devise::RegistrationsController
   respond_to :json
   skip_before_action :authenticate_user_from_token!
+
+  protected
+
+  def sign_up_params
+    params.permit(:email, :password)
+  end
 end
