b4f1fd3a8634590efaad0de059025c6622f611b1
Teemu Y <theme87@gmail.com>
2016-03-17 17:17:29 +0200

    indentation

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,61 +1,60 @@
 class CategoriesController < ApplicationController
-#load_and_authorize_resource
-before_action :set_category, only: [:show, :update, :destroy]
-load_and_authorize_resource
+  before_action :set_category, only: [:show, :update, :destroy]
+  load_and_authorize_resource
 
-# GET /category
-# GET /category.json
-def index
-  @categories = Category.all
-  @categories.each do |category|
-    authorize! :read, category
+  # GET /category
+  # GET /category.json
+  def index
+    @categories = Category.all
+    @categories.each do |category|
+      authorize! :read, category
+    end
+    render json: @categories
   end
-  render json: @categories
-end
 
-# GET /category/1
-# GET /category/1.json
-def show
-  render json: @category
-end
-
-# POST /category
-# POST /category.json
-def create
-  @category = Category.new(category_params)
-  authorize! :create, @category
-  if @category.save
-    render json: @category, status: :created
-  else
-    render json: @category.errors, status: :unprocessable_entity
+  # GET /category/1
+  # GET /category/1.json
+  def show
+    render json: @category
   end
-end
 
-# PATCH/PUT /category/1
-# PATCH/PUT /category/1.json
-def update
-  if @category.update(category_params)
-    render json: @category, status: :ok
-  else
-    render json: @category.errors, status: :unprocessable_entity
+  # POST /category
+  # POST /category.json
+  def create
+    @category = Category.new(category_params)
+    authorize! :create, @category
+    if @category.save
+      render json: @category, status: :created
+    else
+      render json: @category.errors, status: :unprocessable_entity
+    end
   end
-end
 
-# DELETE /category/1
-# DELETE /category/1.json
-def destroy
-  @category.destroy
-  head :no_content
-end
+  # PATCH/PUT /category/1
+  # PATCH/PUT /category/1.json
+  def update
+    if @category.update(category_params)
+      render json: @category, status: :ok
+    else
+      render json: @category.errors, status: :unprocessable_entity
+    end
+  end
 
-private
-# Use callbacks to share common setup or constraints between actions.
-def set_category
-  @category = Category.find(params[:id])
-end
+  # DELETE /category/1
+  # DELETE /category/1.json
+  def destroy
+    @category.destroy
+    head :no_content
+  end
 
-# Never trust parameters from the scary internet, only allow the white list through.
-def category_params
-  params.permit(:title)
-end
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_category
+    @category = Category.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def category_params
+    params.permit(:title)
+  end
 end
