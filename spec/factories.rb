bc537cfe9805027759236ae6c1126ba8c2f0ceaf
Nikita Frolov <nikita@frolov.fi>
2016-04-19 15:33:46 +0300

    Add request tests for batches

--- spec/factories.rb
+++ spec/factories.rb
@@ -11,4 +11,29 @@ FactoryGirl.define do
       teacher true
     end
   end
+
+  factory :exercise do
+    batch
+    title 'Exercise 1'
+    text 'Description'
+  end
+
+  factory :batch do
+    title 'Week 1'
+    deadline { 2.days.from_now }
+
+    trait :expired do
+      deadline { 2.days.ago }
+    end
+
+    factory :batch_with_exercises do
+      transient do
+        exercises_count 5
+      end
+
+      after(:create) do |batch, evaluator|
+        create_list(:exercise, evaluator.exercises_count, batch: batch)
+      end
+    end
+  end
 end
